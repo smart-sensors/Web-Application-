1326fbadf3f410ead1b0ba90ba05a530c316f939
isakamot <isakamot@purdue.edu>
2018-07-03 23:31:57 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,10 +1,18 @@
+#import modules
 from flask import*
 
 app = Flask(__name__)
 
 @app.route('/')
+def brokersetup():
+    return render_template('brokersetup.html')
+@app.route('/homepage')
 def index():
     return render_template('index.html')
 
+@app.route('/setup')
+def setup():
+    return render_template('setup.html')
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host = '0.0.0.0', port = 5000)
